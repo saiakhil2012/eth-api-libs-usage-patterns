134604203ad7f1128bb8eda0763c426c04401633
Bala Siva Sai Akhil Malepati <saiakhil2012@yahoo.com>
2023-05-10 17:18:52 -0700

    Simple reads from Blockchain

---
+++ ethers.js/1-read-from-blockchain.js
@@ -0,0 +1,21 @@
+const { ethers } = require("ethers");
+const config = require('./config-self.json');
+
+// Setting up a JSON RPC Provider
+var provider;
+try {
+  provider = new ethers.providers.JsonRpcProvider(config.jsonrpc.endpoints.https);
+} catch (err) {
+  console.log("Cannot connect to Provider");
+  console.log(err);
+}
+
+const main = async () => {
+  var chainId = await provider.send("eth_chainId", []);
+  console.log(`Chain ID is: ${chainId}`);
+
+  var gasPrice = await provider.send("eth_gasPrice");
+  console.log(`Gas Price is: ${BigInt(gasPrice).toString()} wei`);
+}
+
+main()
