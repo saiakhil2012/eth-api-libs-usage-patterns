499ae784a072884f4fd1266ac28d98c21e0758b6
Bala Siva Sai Akhil Malepati <saiakhil2012@yahoo.com>
2023-05-11 20:58:39 -0700

    Add web3j based pattern for simple generic reading from blockchain
    Signed-off-by: Bala Siva Sai Akhil Malepati <saiakhil2012@yahoo.com>

---
+++ web3j/src/main/java/com/patterns/Pattern1_ReadFromBlockchain.java
@@ -0,0 +1,37 @@
+package com.patterns;
+
+import org.json.JSONObject;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.EthGasPrice;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.FileReader;
+
+public class Pattern1_ReadFromBlockchain {
+
+    public static void main(String[] args) throws Exception {
+        FileReader fileReader = new FileReader("../config/config-self.json");
+        StringBuilder stringBuilder = new StringBuilder();
+        int character;
+        while ((character = fileReader.read()) != -1) {
+            stringBuilder.append((char) character);
+        }
+        String jsonString = stringBuilder.toString();
+        fileReader.close();
+
+        // Parse the JSON string into a JSONObject
+        JSONObject jsonObject = new JSONObject(jsonString);
+
+        String endpoint = jsonObject.getJSONObject("jsonrpc").getJSONObject("endpoints").getString("https");
+
+        Web3j web3j = Web3j.build(new HttpService(endpoint));
+
+        Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
+        System.out.println("clientVersion is " + web3ClientVersion.getWeb3ClientVersion());
+
+        EthGasPrice ethGasPrice = web3j.ethGasPrice().send();
+        System.out.println("gasPrice is " + ethGasPrice.getGasPrice());
+    }
+}
+
